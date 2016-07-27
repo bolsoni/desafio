3267761ca928ea295ffa824277cd5c2c98ff55a5
Leonardo Bolsoni <bolsonileonardo@gmail.com>
2016-07-26 22:16:18 -0300

    fix app name

--- starter.sh
+++ starter.sh
@@ -3,5 +3,5 @@
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
         export PATH=/usr/local/bin:$PATH
-        forever start --sourceDir /usr/local/webservice/node.js >> /var/log/app.log 2>&1
+        forever start --sourceDir /usr/local/webservice/app.js >> /var/log/app.log 2>&1
 fi
